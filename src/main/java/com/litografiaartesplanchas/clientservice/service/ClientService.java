b01321b470ac6794bc8cd13855d4d5158972b1d8
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-03-31 20:32:23 -0500

    LAP-139 - Comments

--- src/main/java/com/litografiaartesplanchas/clientservice/service/ClientService.java
+++ src/main/java/com/litografiaartesplanchas/clientservice/service/ClientService.java
@@ -1,9 +1,11 @@
 package com.litografiaartesplanchas.clientservice.service;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.litografiaartesplanchas.clientservice.model.Client;
@@ -14,11 +16,23 @@ public class ClientService {
 	@Autowired
 	private IClientRepository repository;
 	
+	/**
+	 * The function getAll() returns a list of all clients from the repository.
+	 * 
+	 * @return A List of Client objects is being returned.
+	 */
 	public List<Client> getAll(){
 		return repository.findAll();
 	}
 	
-	public Optional<Client> getClientById(Long id) {
+	/**
+	 * The function returns an Optional containing a Client object found by its ID in the repository.
+	 * 
+	 * @param id The `id` parameter is the unique identifier of the client whose information you want to
+	 * retrieve from the repository.
+	 * @return An Optional object containing a Client entity is being returned.
+	 */
+	public Optional<Client> getClientById(long id) {
 		return repository.findById(id);
 	}
 	
