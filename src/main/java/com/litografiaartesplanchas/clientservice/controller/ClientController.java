65ff1f3bc07fd0c2f2fdac834fb941a40f4f5cf6
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-04-03 15:53:47 -0500

    LAP-133 Implement sign up client endpoint in controller

--- src/main/java/com/litografiaartesplanchas/clientservice/controller/ClientController.java
+++ src/main/java/com/litografiaartesplanchas/clientservice/controller/ClientController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import com.litografiaartesplanchas.clientservice.model.Client;
 import com.litografiaartesplanchas.clientservice.service.ClientService;
 import com.litografiaartesplanchas.clientservice.utils.Response;
 import com.litografiaartesplanchas.clientservice.utils.ResponseBody;
+import com.litografiaartesplanchas.clientservice.utils.errors.ErrorHandlerResponse;
 
 /**
  * The `ClientController` class in Java defines REST endpoints for managing client data and handling
@@ -75,6 +78,30 @@ public class ClientController {
 		}catch(Exception e) {
 			System.out.println(e);
 			return Response.badRequest();
+		}
+	}
+	
+	/**
+	 * This Java function handles a POST request to create a new client and returns a response entity with
+	 * a success or error message.
+	 * 
+	 * @param client The `client` parameter in the `createClient` method is of type `Client`, which is a
+	 * class representing a client entity. It is annotated with `@RequestBody`, indicating that the method
+	 * expects the client information to be passed in the request body when making a POST request to the
+	 * "/signup"
+	 * @return The method is returning a ResponseEntity object with a ResponseBody containing the result
+	 * of the signup operation. If the signup operation is successful, it returns a Response with a null
+	 * body and an OK status. If an exception occurs during the signup operation, it returns a Response
+	 * with the error handled by the ErrorHandlerResponse class.
+	 */
+	@PostMapping(value = "/signup")
+	public ResponseEntity<ResponseBody> createClient(@RequestBody Client client){
+		try{
+			service.create(client);
+			return Response.ok(null);
+
+		}catch(Exception e) {
+			return ErrorHandlerResponse.handleException(e);
 		}
 	}
 	
